55843ca1c4f8b94fc4bd536ff5dea04a0ee3c911
Lastly1999 <1358826554@qq.com>
2021-08-12 01:25:50 +0800

    更新admin基础功能，如vuex分块操作，及vite一些常用配置项

--- src/components/NavMenu/NavMenu.vue
+++ src/components/NavMenu/NavMenu.vue
@@ -1,12 +1,11 @@
 <template>
     <el-menu
         :default-active="activeIndex2"
-        class="el-menu-demo"
-        mode="horizontal"
         @select="handleSelect"
-        background-color="#545c64"
-        text-color="#fff"
-        active-text-color="#ffd04b">
+        background-color="#30426a"
+        text-color="#b2bfdc"
+        active-text-color="#fff"
+    >
         <el-menu-item index="1">处理中心</el-menu-item>
         <el-submenu index="2">
             <template #title>我的工作台</template>
@@ -20,8 +19,6 @@
                 <el-menu-item index="2-4-3">选项3</el-menu-item>
             </el-submenu>
         </el-submenu>
-        <el-menu-item index="3" disabled>消息中心</el-menu-item>
-        <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
     </el-menu>
 </template>
 
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,19 +1,25 @@
 <template>
-    <FastContainer style="height:100%">
-        <FastAside>
-            header
-        </FastAside>
-        <FastContainer>
-            <FastHeader>Header</FastHeader>
-            <FastContent>Main</FastContent>
-            <FastFooter>Footer</FastFooter>
-        </FastContainer>
-    </FastContainer>
+    <el-container style="height:100%">
+        <el-aside style="background-color:#30426a" width="250px">
+            <NavMenu/>
+        </el-aside>
+        <el-container>
+            <el-header>Header</el-header>
+            <el-container>Main</el-container>
+            <el-footer>Footer</el-footer>
+        </el-container>
+    </el-container>
 </template>
 <script lang="ts">
 import {defineComponent} from "vue"
 
+// components
+import NavMenu from '@/components/NavMenu/NavMenu.vue'
+
 export default defineComponent({
+    components: {
+        NavMenu
+    },
     setup() {
     },
 })
