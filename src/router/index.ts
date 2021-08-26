3c1da949b4d1ee67e4f13b0ad7d4053912600865
Lastly1999 <1358826554@qq.com>
2021-08-27 00:26:03 +0800

    根权限更细

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,20 @@
-import {createRouter, createWebHistory} from "vue-router"
+import {
+    createRouter,
+    createWebHistory,
+    RouteLocationNormalized,
+    NavigationGuardNext,
+    NavigationFailure,
+    Router,
+    RouteRecordRaw
+} from "vue-router"
+import { routesConf } from '@/config/router.config'
+import nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
 
-const routes = [
+// vuex
+import store from '@/store'
+
+const routes: RouteRecordRaw[] = [
     {
         path: "/",
         redirect: "/login",
@@ -11,14 +25,36 @@ const routes = [
     },
     {
         path: "/home",
-        component: () => import("@/App"),
+        component: () => import("views/Home/Home.vue"),
+        children: routesConf
     },
 ]
 
 // 构建路由栈
-const router = createRouter({
+const router: Router = createRouter({
     history: createWebHistory(),
     routes,
+})
+
+router.beforeEach(async (to: RouteLocationNormalized, form: RouteLocationNormalized, next: NavigationGuardNext) => {
+    // 如果是登录页 默认放行 不进行权限验证
+    if (to.path === '/login') {
+        next()
+    } else {
+        // todo 权限验证在除开登录页之外pages处理
+        // 进行用户id请求权限菜单操作
+        await store.dispatch('sysModule/API_GET_SYS_MENUS', 1)
+        const roleMenus = JSON.parse(JSON.stringify(store.getters['sysModule/getSysMenus']))
+        
+        next()
+    }
+    // nprogress start...
+    nprogress.start()
+})
+
+router.afterEach((to: RouteLocationNormalized, form: RouteLocationNormalized, next: void | NavigationFailure | undefined) => {
+    // nprogress end...
+    nprogress.done()
 })
 
 export default router
